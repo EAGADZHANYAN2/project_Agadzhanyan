9e277ee935f56c91b89e26c71e0cc180d0a09910
User <Edyardag@mail.ru>
2026-02-06 17:28:21 +0300

    пз

--- Pz_8/Pz_8_1.py
+++ Pz_8/Pz_8_1.py
@@ -3,6 +3,11 @@
 #удалить 'profession', вывести все пары ключ-значение и проверить наличие 'age'.
 
 my_dict = {'name': 'Alice', 'age': 35, 'city': 'New York', 'profession': 'Doctor'}
+
+if 'age' in my_dict:
+    print("Ключ 'age' существует в словаре")
+else:
+    print("Ключа 'age' нет в словаре")
 try:
     my_dict.pop('profession')
     print("Ключ 'profession' успешно удален")
@@ -13,7 +18,3 @@ print("Все пары ключ-значение после удаления:")
 for key, value in my_dict.items():
     print(f"{key}: {value}")
 
-if 'age' in my_dict:
-    print("Ключ 'age' существует в словаре")
-else:
-    print("Ключа 'age' нет в словаре")
